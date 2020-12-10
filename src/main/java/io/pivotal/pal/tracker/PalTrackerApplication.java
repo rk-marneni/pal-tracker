6b1a9d86ea87cf918239509a53c267320776eef5
RK <rk.marneni@fedex.com>
2020-12-10 16:15:03 +0000

    added jdbc functional changes

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,13 @@
 package io.pivotal.pal.tracker;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -12,7 +17,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository timeEntryRepository(DataSource dataSource) {
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
